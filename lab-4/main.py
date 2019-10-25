3e977b5ee09187a54227baa03631e0d400e90010
KrutNA <krutna@pm.me>
2019-10-25 16:42:00 +0300

    Changed 'main.py' and added report

--- lab-4/main.py
+++ lab-4/main.py
@@ -1,31 +1,15 @@
 #!/usr/bin/env python3
-from json_lib import JsonObject, parser as json_parser
+from json_lib.parser import Parser as JsonParser
 from yaml_lib import YamlObject
-from collections import deque
 
 if __name__ == "__main__":
-    init = {"int": 666,
-            "float": 666.777,
-            "string": "String",
-            "array": ["String", 0o666, {"1": True, "2": [0x666, 0o777]}],
-            "empty seq": [],
-            "empty map": {}}
-    txt = """{
-  'int': 666.0,
-  'float': 666.777,
-  'string': "String",
-  'array': [
-    "String",
-    438.0,
-    {
-      '1': true,
-      '2': [
-        1638.0,
-        511.0
-      ]
-    }
-  ],
-  'empty seq': [],
-  'empty map': {}
-}w"""
-    print(json_parser.Parser(txt).parse().to_string())
+    file = "schedule.json"
+    result = None
+    with open(file, "r") as f:
+        result = f.read()
+
+    json = JsonParser(result).parse()
+
+    yaml = YamlObject.from_normal(json.to_normal())
+
+    print(yaml.to_string())
